f556e0a5d063cdfc0f991ea2b3b2da5c2abe1420
NekruzRakhimov <nekruzusa111@gmail.com>
2020-08-19 02:25:18 -0700

    Calculator without eval

---
+++ js/app.js
@@ -0,0 +1,84 @@
+const inputCalc = document.querySelector('.account'), resultCalc = document.querySelector('.result');
+
+function input(i) {
+    inputCalc.value = inputCalc.value + i
+}
+
+function backspace() {
+    inputCalc.value = inputCalc.value.substring(0, inputCalc.value.length - 1);
+}
+
+function reset() {
+    resultCalc.value = '';
+        inputCalc.value = '';
+}
+
+function parseCalculationString(s) {
+    var calculation = [],
+        current = '';
+
+    for (var i = 0, ch; ch = s.charAt(i); i++) {
+        if ('^*/+-'.indexOf(ch) > -1) {
+            if (current == '' && ch == '-') {
+                current = '-';
+            } 
+            else {
+                calculation.push(parseFloat(current), ch);
+                current = '';
+            }
+        } 
+        else {
+            current += s.charAt(i);
+        }
+    }
+    if (current != '') {
+        calculation.push(parseFloat(current));
+    }
+    return calculation;
+}
+
+function calculate(calc) {
+    var ops = [
+        {
+            '^': (a, b) => Math.pow(a, b)
+        },
+
+        {
+            '*': (a, b) => a * b,
+            '/': (a, b) => a / b
+        },
+        
+        {
+            '+': (a, b) => a + b, 
+            '-': (a, b) => a - b
+        }
+    ],
+        newCalc = [],
+        currentOp;
+    for (var i = 0; i < ops.length; i++) {
+        for (var j = 0; j < calc.length; j++) {
+            if (ops[i][calc[j]]) {
+                currentOp = ops[i][calc[j]];
+            } else if (currentOp) {
+                newCalc[newCalc.length - 1] = 
+                    currentOp(newCalc[newCalc.length - 1], calc[j]);
+                currentOp = null;
+            } else {
+                newCalc.push(calc[j]);
+            }
+        }
+        calc = newCalc;
+        newCalc = [];
+    }
+    if (calc.length > 1) {
+        console.log('Error: unable to resolve calculation');
+        return calc;
+    } else {
+        return calc[0];
+    }
+}
+
+function result() {
+    resultCalc.value = calculate(parseCalculationString(inputCalc.value));
+    inputCalc.value = calculate(parseCalculationString(inputCalc.value));
+}
